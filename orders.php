4283269eda110b41e930a04e835fb9696ffa348d
nahed <nahed581213@gmail.com>
2023-01-09 09:06:00 +0200

    first_commit

---
+++ orders.php
@@ -0,0 +1,134 @@
+<?php
+require_once ('./connection.php');
+$query = "SELECT * FROM `orders` limit 5";
+    $sql = $con->prepare($query);
+    $result = $sql->execute();
+    $data = $sql->fetchall(PDO::FETCH_ASSOC);
+    // print_r($data) ;
+    $query_user = "SELECT `name` FROM `user`,`orders` WHERE user.Id=orders.id_user ORDER BY orders.Id limit 5;";
+    $sql_user = $con->prepare($query_user);
+    $result_user = $sql_user->execute();
+    $data_user = $sql_user->fetchall(PDO::FETCH_ASSOC);
+    // print_r($data_user);
+?>
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="img/fav.png">
+    <!-- Author Meta -->
+    <meta name="author" content="codepixer">
+    <!-- Meta Description -->
+    <meta name="description" content="">
+    <!-- Meta Keyword -->
+    <meta name="keywords" content="">
+    <!-- meta character set -->
+    <meta charset="UTF-8">
+    <!-- Site Title -->
+    <title>Coffee</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+    <!--
+			CSS
+			============================================= -->
+    <link rel="stylesheet" href="css/linearicons.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/nice-select.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/mycss.css">
+    
+</head>
+
+<body>
+
+    <header id="header" id="home">
+        <div class="header-top">
+            <div class="container">
+                <div class="row justify-content-end">
+                    <div class="col-lg-8 col-sm-4 col-8 header-top-right no-padding">
+                        <ul>
+                            <li>
+                                Mon-Fri: 8am to 2pm
+                            </li>
+                            <li>
+                                Sat-Sun: 11am to 4pm
+                            </li>
+                            <li>
+                                <a href="tel:(012) 6985 236 7512">(012) 6985 236 7512</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row align-items-center justify-content-between d-flex">
+                <div id="logo">
+                    <a href="index.html"><img src="img/logo.png" alt="" title="" /></a>
+                </div>
+                <nav id="nav-menu-container">
+                    <ul class="nav-menu">
+                        <li class="menu-active"><a href="#home">Home</a></li>
+                        <li><a href="#products">Products</a></li>
+                        <li><a href="#coffee">Users</a></li>
+                        <li><a href="#review">Manual order</a></li>
+                        <li><a href="#blog">Checks</a></li>
+                    </ul>
+                </nav><!-- #nav-menu-container -->
+            
+                <img id="img1" src="" alt="admin_pic">
+                <a id="a1" href="">Admin</a>
+            </div>
+        </div>
+    </header><!-- #header -->
+
+
+
+
+<section class="sec1">
+<h2>Orders</h2>
+<table>
+<tr>  
+    <th>Order date</th>
+    <th>Name</th>
+    <th>Room</th>
+    <th>Action</th>
+</tr>
+
+
+</section>
+
+
+
+    <!-- End footer Area -->
+
+    <script src="js/vendor/jquery-2.2.4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="js/vendor/bootstrap.min.js"></script>
+    <script type="text/javascript"
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+    <script src="js/easing.min.js"></script>
+    <script src="js/hoverIntent.js"></script>
+    <script src="js/superfish.min.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/parallax.min.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/mail-script.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
